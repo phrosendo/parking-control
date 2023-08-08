f08c180d5fe3d2ae94b11bc15be1fa4c42f5de05
Pedro Henrique Rosendo da Silva <pedro.ads@otlook.com>
2023-08-07 23:26:39 -0300

    Alteração no dto do ParkingSpotDTO

--- src/main/java/com/api/parking_control/dtos/ParkingSpotDTO.java
+++ src/main/java/com/api/parking_control/dtos/ParkingSpotDTO.java
@@ -1,9 +1,10 @@
 package com.api.parking_control.dtos;
 
 import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Data
